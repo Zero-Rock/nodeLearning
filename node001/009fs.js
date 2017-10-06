8bb45049607903be7828667d6f024e145fb390a6
mobius <mobius_pan@yeah.net>
2017-10-06 11:14:15 +0800

    file system

--- node001/009fs.js
+++ node001/009fs.js
@@ -2,3 +2,14 @@
  * Created by Mobius on 2017/9/28 23:10.
  */
 const fs = require("fs");
+/**
+ * 异步读取文件
+ * fs.readFile(path[, options], callback)
+ */
+fs.readFile("../test/user.txt","UTF-8",(error,data)=>{
+    if(error){
+        console.log(error)
+    }
+    console.log(data)
+})
+console.log("complate")
