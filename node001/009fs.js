e92acf400b1558b3e8435e6677c3821111d28df2
mobius <mobius_pan@yeah.net>
2017-10-06 12:54:29 +0800

    file system

--- node001/009fs.js
+++ node001/009fs.js
@@ -5,11 +5,26 @@ const fs = require("fs");
 /**
  * 异步读取文件
  * fs.readFile(path[, options], callback)
+ * 若不指定编码，则返回buffer对象
  */
-fs.readFile("../test/user.txt","UTF-8",(error,data)=>{
-    if(error){
-        console.log(error)
+fs.readFile("../test/user.txt", "UTF-8", (error, data) => {
+    if (error) {
+        console.log(error);
     }
-    console.log(data)
+    console.log(data);
+});
+console.log("complate");
+/**
+ * 同步读取文件
+ * fs.readFileSync(path[, options])
+ * 若不指定编码，则返回buffer对象
+ */
+const data = fs.readFileSync("../test/user2.txt", "utf-8");
+console.log(data);
+fs.readlink("../test/user.txt",(e,d)=>{
+    if (e){
+        console.log(e);
+        return ;
+    }
+    console.log(d)
 })
-console.log("complate")
