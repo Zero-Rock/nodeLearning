45048cfb8362ef9a9a5b8e3c8a106e01a408f8e6
mobius <mobius_pan@yeah.net>
2017-10-15 10:50:08 +0800

    template

--- node001/013express.js
+++ node001/013express.js
@@ -5,17 +5,15 @@ const express = require("express");
 const queryString = require("querystring");
 const path = require("path");
 const app = express();
+//请求 localhost:1234/index?name=mobius
 app.get("/index", (req, res) => {
-    res.send(req.query.username + "欢迎光临");
+    res.send(`${req.query.name}欢迎光临1`);
+    console.log(req.params)
 });
-//*代表所有路径,都识别
-app.get("*", (req, res) => {
-    console.log(req.path);
-    //向客户端返回文件
-    //注:文件路径 为绝对路径
-    console.log(path.resolve(__dirname + "./../test/user.txt"))
-    res.sendFile(path.resolve(__dirname + "./../test/user.txt"))
-})
+//请求 localhost:1234/mobius
+app.get("/index/:name", (req, res) => {
+    res.send(`${req.params.name}欢迎光临2`);
+});
 app.post('/adult', (req, res) => {
     var pathData = "";
     req.on("data", (chunk) => {
@@ -27,5 +25,27 @@ app.post('/adult', (req, res) => {
         res.send("你的年龄是" + obj.age);
     })
 });
+app.set("view engine", "ejs");
+
+//设置模板引擎的位置
+app.set("views", path.resolve(__dirname, "./../test"));
+app.get("/welcome/:name/:gender/:age", (req, res) => {
+    console.log(req.params)
+    //res.sendFile(__dirname+req.path);
+    //返回模板(默认后缀ejs)
+    res.render("013express", {
+        name: req.params.name,
+        gender: req.params.gender,
+        age: req.params.age,
+    })
+})
+//*代表所有路径,都识别
+app.get("*", (req, res) => {
+    console.log(req.path);
+    //向客户端返回文件
+    //注:文件路径 为绝对路径
+    console.log(path.resolve(__dirname + "./../test/user.txt"))
+    res.sendFile(path.resolve(__dirname + "./../test/user.txt"))
+})
 app.listen(1234);
 console.log("service start")
